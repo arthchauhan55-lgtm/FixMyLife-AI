ead349c97d9c04deed47eb0af3c94b0aa4d0b575
arthchauhan55-lgtm <arthchauhan55@gmail.com>
2026-05-25 06:15:10 +0530

    Add Redis configuration

---
+++ backend/src/config/redis.js
@@ -0,0 +1,18 @@
+const redis = require('redis');
+const logger = require('../utils/logger');
+
+const client = redis.createClient({
+  url: process.env.REDIS_URL || 'redis://localhost:6379',
+});
+
+client.on('error', (err) => {
+  logger.error('Redis Client Error', err);
+});
+
+client.on('connect', () => {
+  logger.info('Redis connected');
+});
+
+client.connect();
+
+module.exports = client;
